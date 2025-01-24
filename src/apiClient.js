76791deb16546b8bac36b03a83996bd3d753a56a
[CLOUD4] 안창민 <chm20060@gmail.com>
2025-01-24 13:57:50 +0900

    fix: 배포 중 403에러 해결

--- src/apiClient.js
+++ src/apiClient.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // API 클라이언트 생성
 export const apiClient = axios.create({
-    baseURL: 'api',  // baseURL은 '/'로 시작해야 합니다.
+    baseURL: '/api',  // baseURL은 '/'로 시작해야 합니다.
 });
