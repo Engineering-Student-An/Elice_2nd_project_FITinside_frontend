b344a7720dcd4ed177ec9a06146b8f42164f8f1a
[CLOUD4] 안창민 <chm20060@gmail.com>
2025-01-24 17:39:29 +0900

    fix: 배포 중 403 에러 해결 apiClient 사용으로 변경

--- src/apiClient.js
+++ src/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const host = window.location.hostname === "localhost"
     ? `http://ec2-13-209-198-107.ap-northeast-2.compute.amazonaws.com:8080/api`
-    : "api";
+    : "/api";
 
 
 // API 클라이언트 생성
